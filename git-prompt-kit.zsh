4a168efd383a4a4c487d119f8195f7e644492834
Henry Bley-Vroman <olets@olets.dev>
2024-06-02 08:52:09 -1000

    chore(bump): bump to 4.1.2, update changelog
    >>> conventional-changelog -i CHANGELOG.md -s -p angular

--- git-prompt-kit.zsh
+++ git-prompt-kit.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
 # Git Prompt Kit
-# v4.1.2
-# March 26 2024
+# v4.1.3
+# June 2 2024
 # https://github.com/olets/git-prompt-kit
 # Copyright (©) 2019-present Henry Bley-Vroman
 
-typeset -r GIT_PROMPT_KIT_VERSION="4.1.2"
+typeset -r GIT_PROMPT_KIT_VERSION="4.1.3"
 
 # Behavior options
 typeset -gi GIT_PROMPT_KIT_HIDE_INACTIVE_AHEAD_BEHIND=${GIT_PROMPT_KIT_HIDE_INACTIVE_AHEAD_BEHIND:-1}
